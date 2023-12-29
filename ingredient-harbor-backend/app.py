85295de8cf164bc603f9cf09ea1c1099d00552dc
KevinD <kevindel891@gmail.com>
2023-12-29 03:36:06 -0800

    update regex Expression to include .

--- ingredient-harbor-backend/app.py
+++ ingredient-harbor-backend/app.py
@@ -21,12 +21,41 @@ def staticScrape(site, key, listType):
 
     for ingredient in ingredientList:
         processedIngredient = ingredient.replace('\n', '')
+        ingredientSplit = re.match("^([\d/]+ [a-zA-Z]+[.]?) (.+)$", processedIngredient)
+        if(ingredientSplit):
+            dividedIngredientList.append([ingredientSplit.group(1), ingredientSplit.group(2)])
+        else:
+            dividedIngredientList.append(processedIngredient)
+    return(dividedIngredientList)
+
+def dynamicScrape(site, key, listType):
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    driver.get(site)
+    page_source = driver.page_source
+    driver.close()
+
+    page_source_dynamic = BeautifulSoup(page_source, "lxml")
+    found = page_source_dynamic.find_all(listType, key)
+    ingredientList = []
+    dividedIngredientList = []
+    for ingredient in found:
+        if(ingredient.text != 'Deselect All' and ingredient.text != 'Select All'):
+            ingredientList.append(ingredient.text)
+
+    for ingredient in ingredientList:
+        processedIngredient = ingredient.replace('\n', '')
         ingredientSplit = re.match("^([\d/]+ [a-zA-Z]+) (.+)$", processedIngredient)
         if(ingredientSplit):
             dividedIngredientList.append([ingredientSplit.group(1), ingredientSplit.group(2)])
         else:
             dividedIngredientList.append(processedIngredient)
     return(dividedIngredientList)
+
 
 def getSiteName(site):
     regexPattern = re.search('[.][a-zA-z]+[.]', site)
@@ -46,7 +75,14 @@ def checkValidMeasurements(inputtedList):
         "cup",
         "pint",
         "quart",
-        "gallon"
+        "gallon",
+        "tbsp.",
+        "tbsp",
+        "tsp",
+        "tsp.",
+        "qrt.",
+        "qrt"
+
     ]
     
     if(inputtedList.split()[1] in valid_measurements):
@@ -68,6 +104,8 @@ def test():
     listType = formatData['singleKey'][site_name]["ingredientListType"]
     if(checkStatic(site_link, key)):
         return(staticScrape(site_link, key, listType))
+    else:
+        return(dynamicScrape(site_link, key, listType))
 
     
 
